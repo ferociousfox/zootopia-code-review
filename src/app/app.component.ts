f501dc08b6bf392e006de89c918f89dd26b97f5b
shane ryan <onyx.shane@gmail.com>
2018-01-19 09:34:20 -0800

    add all components needed to finish app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Animal } from './animal-model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Zootopia';
+  // SelectedAnimal
+  masterAnimals: Animal[] = [
+      {species: "Arctic Fox", name: "Moon", age:2, diet: "canivore", location: "Northern trail",  caretakers: 5, sex: "female", likes: "cold", dislikes: "Loud noises"},
+      {species: "Ocelot", name: "Prince", age:4, diet: "canivore", location: "Jungel trail",  caretakers: 6, sex: "male", likes: "Murder", dislikes: "stupidity"},
+      {species: "Coyote", name: "Trickster", age:100, diet: "omnivorous", location: "Northernwest trail?",  caretakers: 10, sex: "unknown", likes: "New things", dislikes: "Complacency"}
+  ]
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AnimalListComponent } from './animal-list/animal-list.component';
+import { AnimalEditComponent } from './animal-edit/animal-edit.component';
+import { AnimalNewComponent } from './animal-new/animal-new.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimalListComponent,
+    AnimalEditComponent,
+    AnimalNewComponent
   ],
   imports: [
     BrowserModule,
